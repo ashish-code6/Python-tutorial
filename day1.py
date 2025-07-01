0788b2268ecc318376bb267cd442d951a145fdcc
ashish-code6 <ashishk810222@gmail.com>
2025-07-01 22:32:49 +0530

    day1

--- day1.py
+++ day1.py
@@ -1,2 +1,39 @@
-print("Hello world");
+print("Hello Python!!!");
+
+
+#data Types and variable
+name="Ashish";
+Work="None";
+job="True"
+salary=56000.97;
+
+print(type(name),name)
+print(type(Work),Work)
+print(type(job),job)
+print(type(salary),salary)
+
+# Python is a case sensetive language.
+
+# Python in Arthmatic Operator
+
+a=5;
+b=6;
+print(a+b);
+print(a*b);
+print(a-b);
+print(a/b);
+print(a%b);
+print(a**b);
+
+
+#type Casting
+
+x=int("2")
+y=9;
+sum=x+y;
+print(sum)
+
+
+
+
 
