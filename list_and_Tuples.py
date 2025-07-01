c51836c437d6b22b15550158cb0b6af1d64e769d
ashish-code6 <ashishk810222@gmail.com>
2025-07-02 00:03:07 +0530

    list

---
+++ list_and_Tuples.py
@@ -0,0 +1,16 @@
+# List 
+
+student=["Suresh","17","Python","Bangaluru"]
+
+print(student)
+
+print(student[2])
+
+student[2]="JavaScript"
+
+print(student)
+
+#list is a immutable  
+
+
+# in list also slicing is possible
