612c1e7410b770fa243bf9800cfffb39a48df69c
ashish-code6 <ashishk810222@gmail.com>
2025-07-02 21:38:52 +0530

    list

--- list_and_Tuples.py
+++ list_and_Tuples.py
@@ -11,6 +11,33 @@ student[2]="JavaScript"
 print(student)
 
 #list is a immutable  
-
-
 # in list also slicing is possible
+
+#------- list In Method-------
+
+list=[4,8,6,2,5]
+
+#append method is used to add the value at the last list.
+
+list.append(9);
+
+#sorting in a List -------
+
+list.sort()
+#-----sorting a list in a reverse oreder-------
+list.sort(reverse=True)
+
+
+print(list.reverse())
+
+list.reverse()
+
+# add at particular index
+
+list.insert(4,'a')
+
+list.pop()
+print(list)
+
+
+
